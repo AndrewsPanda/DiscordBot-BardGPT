8cf45b3c37dffa003da0231fdcae4f4ca97c85b5
AndrewsPanda <acmcdowell01@gmail.com>
2023-07-20 07:07:09 +1000

    Update help.py

--- cogs/help.py
+++ cogs/help.py
@@ -5,7 +5,7 @@ from discord import app_commands
 class Help(Cog_Extension):
     @app_commands.command(name = "help", description = "Show how to use")
     async def help(self, interaction: discord.Interaction):
-        embed=discord.Embed(title="Help", description="[see more](https://github.com/FuseFairy/DiscordBot-BardGPT/blob/main/requirements.txt)\n\n**COMMANDS -**")
+        embed=discord.Embed(title="Help - link below", description="[Click Here to Learn How to Set Cookies](https://docs.google.com/document/d/12N82-QlmWQVPLNswnE7w15WU8cb1YAOCYQuIyec5B3A/edit?usp=sharing)\n\n**COMMANDS -**")
         embed.add_field(name="/bard_cookies", value="Set and delete your Google Bard Cookies.", inline=False)
         embed.add_field(name="/bard", value="Chat with Google Bard.", inline=False)
         embed.add_field(name="/reset_bard_conversation", value="Reset chatbot conversation.", inline=False)
